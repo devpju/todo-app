7c829427064f3a8e1d018099ca3a7e7ca0cbd7ea
Trinh Hoai Duc <anhduc08768@gmail.com>
2024-07-31 15:12:15 +0700

    save todos to local storage

--- src/contexts/AppProvider.jsx
+++ src/contexts/AppProvider.jsx
@@ -1,4 +1,4 @@
-import { createContext, useState } from 'react';
+import { createContext, useState, useEffect } from 'react';
 import { PropTypes } from 'prop-types';
 export const AppContext = createContext();
 
@@ -6,9 +6,16 @@ export const AppProvider = ({ children }) => {
   const [selectedCategoryId, setSelectedCategoryId] = useState();
   const [showSidebar, setShowSidebar] = useState(false);
   const [selectedFilterId, setSelectedFilterId] = useState('all');
-  const [todos, setTodos] = useState([]);
+  const [todos, setTodos] = useState(() => {
+    const savedTodos = localStorage.getItem('todos');
+    return savedTodos ? JSON.parse(savedTodos) : [];
+  });
   const [activeTodoId, setActiveTodoId] = useState('');
   const [searchText, setSearchText] = useState('');
+
+  useEffect(() => {
+    localStorage.setItem('todos', JSON.stringify(todos));
+  }, [todos]);
 
   function handleCompletedTodo(todoId) {
     const updatedTodos = todos.map((todo) => {
